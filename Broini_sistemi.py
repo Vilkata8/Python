f89eb5c6ba4fb2ff4737544ed42a0fbb20e8b2cc
Vilkata8 <115402140+Vilkata8@users.noreply.github.com>
2022-10-26 18:35:32 +0300

    Create Broini_sistemi.py

---
+++ Broini_sistemi.py
@@ -0,0 +1,6 @@
+print("Chisloto 121222208 ot 10chna v 2chna broina sistema e: 111001110011011010001000000 ")
+print("Chisloto 111001110011011010001000000 ot 2chna v 8chna broina sistema e: 716332100 ")
+print("Chisloto 111001110011011010001000000 ot 2chna v 16chna broina sistema e: 739B440")
+print("Chisloto 20102022 ot 10chna v 2chna broina sistema e: 10011001001001111101110000110 ")
+print("Chisloto 10011001001001111101110000110 ot 2chna v 8chna broina sistema e: 114535606 ")
+print("Chisloto 10011001001001111101110000110 ot 2chna v 16chna broina sistema e: 132BB86")
