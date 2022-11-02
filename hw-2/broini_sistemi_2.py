9b524ad8c61d5b726a213cd63e6b9e078786ff18
Velizar Dachenski <vdachenski@tu-sofia.bg>
2022-11-02 12:52:22 +0200

    make changes on file

--- hw-2/broini_sistemi_2.py
+++ hw-2/broini_sistemi_2.py
@@ -1,18 +1,9 @@
 import string
-
-symbols = string.digits + string.ascii_uppercase
-number = 27102022
-binary_num = ""
-hex_num = ""
-
-while number != 0:
-    binary_num += str(number % 2)
-    number //= 2
-
-number = 27102022
-while number != 0:
-    hex_num += symbols[number % 16]
-    number //= 16
-
-print(binary_num[::-1])
-print(hex_num[::-1])
+base=string.digits + string.ascii_letters
+a=int(input('Input number: '))
+b=int(input('Input base: '))
+result=''
+while a//b !=0:
+    result += base[a%b]
+    a//=b
+print(result[::-1])
