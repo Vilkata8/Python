70c08e4cdbf171884866352ed8131e4536389059
Velizar Dachenski <vdachenski@tu-sofia.bg>
2022-11-24 18:56:35 +0200

    Make corrections on zad-2-dop i zad-3-dop

--- hw-5/2 - dop - zadacha.py
+++ hw-5/2 - dop - zadacha.py
@@ -6,12 +6,15 @@ def list_avg(lst):
     for i in range(counter):
         n_in = input(f"Vuvedete element {i+1}: ")
         lst.append(n_in)
-        if n_in.isnumeric() or n_in.isdigit():
+        if type(n_in) == int or type(n_in) == float:
             b.append(n_in)
             suma += float(n_in)
-        aver_1 = suma / len(b)
+            aver_1 = suma / len(b)
+        elif type(n_in) == str and n_in.isnumeric():
+            b.append(float(n_in))
+            suma += float(n_in)
+            aver_1 = suma / len(b)
     print("Spisuk:", b)
     return aver_1
 
 print("Srednoaritmetichnoto na dvete chisla e:", list_avg(a))
-# metodi za dobavqne 
--- hw-5/3 - dop - zadacha.py
+++ hw-5/3 - dop - zadacha.py
@@ -1,12 +1,15 @@
 def digitize(m):
     l_1=[]
-    for i in str(m):
-        l_1.append(int(i))
-    return tuple(l_1)
-m=int(input("Vuvedete chislo: "))
+    if type(m) == str and m.isnumeric():
+        for i in str(m):
+            l_1.append(int(i))
+        return tuple(l_1)
+    	
+    else:
+        print("Vuveli ste greshen tip danni!")
+        return [0,0,0,0]
 
-if type(m) != int:
-    print("Vuveli ste greshen tip danni!")
+m=input("Vuvedete chislo: ")
 
 a,b,c,d=digitize(m)
 print("A e:",a)
@@ -14,4 +17,5 @@ print("B e:",b)
 print("C e:",c)
 print("D e:",d)
 print(a,b,c,d)
-#imam problem s greshkata, koqto da izvede, ako ne e vuvedeno integer 
+
+# chisloto, koeto vuvejdame e str, zashtoto po tozi nachin edinstveno raboti greshkata za vuveden tekst. Gornata proverka pravi proverka dali chisloto e string i  dali e chislo. Ako e, izpulnqva uslovieto. Kodut raboti, razlikata e, che ne raboti s integer, a sus string.
