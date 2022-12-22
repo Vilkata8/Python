e4bad92758e9602bfc64fd205f02f3cad02cccad
Vilkata8 <vdachenski@tu-sofia.bg>
2022-12-22 10:45:05 +0200

    Make corrections on zad Game, but the Game is not working again ;(

---
+++ hw-8/Game/exceptions.py
@@ -0,0 +1,25 @@
+#def character_exist():
+class CharacterExists(Exception):
+    pass
+
+#def character_n_f():
+class CharacterNotFound(Exception):
+    pass
+
+#def inv_character():
+class InvalidCharacter(Exception):
+    pass
+
+#def invalid_commands():
+class InvalidCommand(Exception):
+    pass
+
+#def invalid_param():
+class InvalidParameters(Exception):
+    pass
+
+#def invalid_sex():
+class InvalidSex(Exception):
+    pass
+
+
--- hw-8/Game/main.py
+++ hw-8/Game/main.py
@@ -1,6 +1,6 @@
 #ne zasicha exception-ite ot faila exceptions
 
-from exceptions import *
+#from exceptions import *
 # from exceptions import InvalidParameters
 # from exceptions import CharacterExists
 # from exceptions import InvalidCharacter
@@ -14,18 +14,18 @@ from exceptions import *
 # exceptions.invalid_param()
 # exceptions.invalid_sex()
 
-#ne zasicha exception-ite ot faila exceptions
 
 characters_names = []
 characters = []
-characters_class = ["Magic", "Prince", "King", "Master"]
+characters_class = ["Warrior", "Mage", "Priest", "Rogue"]
 class Menu:
-    def print_menu(self):
+    def print_commands(self):
         print("1. Create a new character.")
         print("2. Create a weapon for existing character.")
         print("3. Create an additional weapon for existing character.")
         print("4. Print all existing characters.")
         print("5. Delete an existing character.")
+        print("6. Close game.")
 
     def command_create_character(self, name, sex, ch_class, weapon=None, additional_weapon=None):
         return name, sex, ch_class
@@ -47,10 +47,11 @@ class Menu:
     def run(self):
         # infinite menu loop
         while True:
-            Menu.print_menu(self)
-            
+            Menu.print_commands(self)   
+        
             # ask the user to choose a command
-            choice = input("Choose an item from the menu: \n> ")
+
+            choice = input("Choose an item from the menu: ")
 
             # catch errors
             try:
@@ -60,15 +61,19 @@ class Menu:
                     # ask the user to input the necessary command parameters
                     name = input("Enter the character name (alpha-numeric): ")
                     if name in characters_names:
-                        raise CharacterExists("Error: Character exists")
+                        raise CharacterExists("Character exists!")
+
                     sex = input("Enter the character sex(Male or Female): ")
                     if sex != "Male" and sex != "Female":
                         raise InvalidSex("Error: Invalid sex")
+
                     ch_class = input("Enter the character class(Warrior, Mage, Priest, Rogue): ")
                     if name.isdigit() or sex.isdigit() or ch_class.isdigit():
-                        raise InvalidParameters("Error: Not supporting numbers!")
+                    #tozi if vinagi vliza v error, koito ot druga strana ne iska da se importne
+                        raise InvalidParameters("Not supporting numbers!")
+
                     if ch_class not in characters_class:
-                        raise InvalidCharacter("Error: Invalid character!")
+                        raise InvalidCharacter("Invalid character!")
 
                     characters_names.append(name)
                     char = self.command_create_character(name, sex, ch_class)
@@ -81,34 +86,35 @@ class Menu:
                     if name in characters_names:
                         weapon = input("Enter a weapon: ")
                         weap = self.command_create_weapon(name, weapon)
-                        print(f"{name} using {weap}\n")
+                        print(f"{name} using {weap}")
                         continue
                     else:
-                        raise CharacterNotFound("Error: Character not found")
+                        raise CharacterNotFound("Character not found!")
 
                 if choice == "3":
                     name = input("Enter the character name (alpha-numeric): ")
                     if name in characters_names:
                         additional_weapon = input("Enter an additional weapon: ")
                         add_weapon = self.command_additional_weapon(name, additional_weapon)
-                        print(f"To character named {name} added additional weapon {add_weapon}\n")
+                        print(f"To character named {name} added additional weapon {add_weapon}")
                         continue
                     else:
-                        raise CharacterNotFound("Error: Character not found")
+                        raise CharacterNotFound("Character not found!")
 
                 if choice == "4":
-                    print(f"{characters} \n")
+                    print(f"The characters:{characters}.")
                     continue
 
                 if choice == "5":
                     characters.clear()
-                    print(f"{characters} All characters deleted! \n")
+                    print(f"{characters} All characters deleted!")
                     continue
 
-                if choice == "Exit":
+                if choice == "6":
+                    print('Goodbye!')
                     break
                 else:
-                    raise InvalidCommand("Error: Invalid choice")
+                    raise InvalidCommand("Invalid choice!")
 
             except Exception as ex:
                 print(f"Error: {str(ex)}")
